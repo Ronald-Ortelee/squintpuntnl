f32d5b2eff6245bad3416b3c749b8cf469a435b8
ronald <ronald@squint.nl>
2016-06-28 20:00:06 +0200

    change

--- functions.php
+++ functions.php
@@ -487,6 +487,12 @@ if ( is_page( 'porto' ) ) {
 
 }}
 
+//* Remove the site footer
+remove_action( 'genesis_footer', 'genesis_footer_markup_open', 5 );
+remove_action( 'genesis_footer', 'genesis_do_footer' );
+remove_action( 'genesis_footer', 'genesis_footer_markup_close', 15 );
+
+
 
 
 
